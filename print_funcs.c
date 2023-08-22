535b0625e35b01745ff8e8de0209139dcc3de0fa
Madona Ramsis <Madona.Adeeb@hotmail.com>
2023-08-22 05:33:08 +0300

    match betty requirements

--- print_funcs.c
+++ print_funcs.c
@@ -44,7 +44,7 @@ int print_s(va_list args)
 }
 
 /**
- * print_s - produces output according to a format.
+ * print_i - prints integers.
  *
  * @args: a va_list that holds the variables.
  *
@@ -53,47 +53,40 @@ int print_s(va_list args)
 int print_i(va_list args)
 {
 	int temp, num, i, digits = 0;
-	char* str;
+	char *str;
 
-	num = va_arg(args, int);
-	temp = num;
+	num = va_arg(args, int), temp = num;
 	if (num == 0)
 	{
 		write(1, "0", 1);
 		return (1);
 	}
-	else 
+	else
 	{
-		while (temp != 0) 
-		{
-			temp /= 10;
-			digits++;
-		}
+		while (temp != 0)
+			temp /= 10, digits++;
 	}
-	str = (char*)malloc((digits + 1) * sizeof(char));
+	str = (char *)malloc((digits + 1) * sizeof(char));
 	if (num < 0)
 	{
 		num = num * -1;
-		str = (char*)malloc((digits + 2) * sizeof(char));
+		str = (char *)malloc((digits + 2) * sizeof(char));
 		str[0] = '-';
-		digits++;
-		str[digits] = '\0';
+		digits++, str[digits] = '\0';
 	}
 	else
 	{
 		str[digits] = '\0';
 	}
-	while (num != 0) 
+	while (num != 0)
 	{
-		digits--;
-		str[digits] = (char)((num % 10) + '0');
+		digits--, str[digits] = (char)((num % 10) + '0');
 		num /= 10;
 	}
 	i = 0;
 	while (str[i] != '\0')
 	{
-		write(1, &str[i], 1);
-		i++;
+		write(1, &str[i], 1), i++;
 	}
 	free(str);
 	return (i);
